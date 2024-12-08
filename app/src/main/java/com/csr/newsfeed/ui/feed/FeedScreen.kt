d76238b710a532b1366179f3d828be1c513ae2fd
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-08 17:00:27 -0300

    Creates basic structure for feed screen

--- app/src/main/java/com/csr/newsfeed/ui/feed/FeedScreen.kt
+++ app/src/main/java/com/csr/newsfeed/ui/feed/FeedScreen.kt
@@ -1,10 +1,31 @@
 package com.csr.newsfeed.ui.feed
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.csr.newsfeed.ui.feed.components.headline.FeedHeadlineComponent
+import com.csr.newsfeed.ui.feed.components.list.ArticlesListComponent
 
 @Composable
-fun FeedScreenHoisting(modifier: Modifier = Modifier) {
-    Text("Alo")
+fun FeedScreenHoisting() {
+    FeedScreen()
+}
+
+@Composable
+private fun FeedScreen(modifier: Modifier = Modifier) {
+    Scaffold { internalPadding ->
+        Surface(modifier = modifier.padding(internalPadding)) {
+            Column(modifier = modifier.padding(horizontal = 16.dp)) {
+                FeedHeadlineComponent(source = "Source xD")
+                Spacer(modifier = modifier.height(16.dp))
+                ArticlesListComponent()
+            }
+        }
+    }
 }
