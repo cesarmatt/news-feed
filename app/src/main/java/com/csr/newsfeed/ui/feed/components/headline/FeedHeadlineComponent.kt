ddb56d14929e008e08618fa903e0429ff3f04665
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-08 17:00:44 -0300

    Changes hierarchy for padding within FeedHeadlineComponent

--- app/src/main/java/com/csr/newsfeed/ui/feed/components/headline/FeedHeadlineComponent.kt
+++ app/src/main/java/com/csr/newsfeed/ui/feed/components/headline/FeedHeadlineComponent.kt
@@ -1,13 +1,17 @@
-package com.csr.newsfeed.ui.feed.components
+package com.csr.newsfeed.ui.feed.components.headline
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun FeedHeadlineComponent(source: String) {
+fun FeedHeadlineComponent(modifier: Modifier = Modifier, source: String) {
     Text(
+        modifier = modifier.padding(top = 16.dp),
         style = MaterialTheme.typography.headlineMedium,
         text = source
     )
