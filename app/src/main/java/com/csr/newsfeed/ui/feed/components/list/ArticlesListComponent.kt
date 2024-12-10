9321464a9117adebb5fefbf52c3cb009afb1fbd3
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-10 19:48:41 -0300

    Removes pagination references

--- app/src/main/java/com/csr/newsfeed/ui/feed/components/list/ArticlesListComponent.kt
+++ app/src/main/java/com/csr/newsfeed/ui/feed/components/list/ArticlesListComponent.kt
@@ -3,52 +3,38 @@ package com.csr.newsfeed.ui.feed.components.list
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.paging.compose.itemKey
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.paging.LoadState
-import androidx.paging.PagingData
-import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.csr.newsfeed.data.Article
-import kotlinx.coroutines.flow.flowOf
+import com.csr.newsfeed.ui.feed.data.FeedUIState
 
 @Composable
 fun ArticlesListComponent(
     modifier: Modifier = Modifier,
-    articles: LazyPagingItems<Article>,
+    uiState: FeedUIState
 ) {
-    when (articles.loadState.refresh) {
-        LoadState.Loading -> {
-            Text("Loading...")
-        }
-        is LoadState.Error -> {
-            Text("Error")
-        }
-        is LoadState.NotLoading -> {
+    when (uiState) {
+        FeedUIState.Loading -> {}
+        is FeedUIState.Error -> {}
+        is FeedUIState.Success -> {
+            val articles = uiState.articles
             LazyColumn {
-                items(
-                    count = articles.itemCount,
-                    key = articles.itemKey { it.uuid },
-                ) { index ->
-                    val item = articles[index]
-                    if (item != null) {
-                        ArticleItemComponent(
-                            article = item
-                        ) { }
-                        Spacer(modifier = modifier.height(16.dp))
-                    }
+                items(items = articles, key = { it.uuid }) {
+                    ArticleItemComponentHoisting(
+                        article = it
+                    ) { }
+                    Spacer(modifier = modifier.height(16.dp))
                 }
             }
         }
+        else -> return
     }
 }
 
 @Preview
 @Composable
 private fun ArticlesListPreview() {
-    ArticlesListComponent(articles = flowOf(PagingData.empty<Article>()).collectAsLazyPagingItems())
+    ArticlesListComponent(uiState = FeedUIState.Idle)
 }
