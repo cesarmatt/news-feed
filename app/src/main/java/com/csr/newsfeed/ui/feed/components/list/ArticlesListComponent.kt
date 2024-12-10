3b9569a92675f000328276df4b34d17fa3e1b70f
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-10 10:53:09 -0300

    Creates logic to deal with paginated lists

--- app/src/main/java/com/csr/newsfeed/ui/feed/components/list/ArticlesListComponent.kt
+++ app/src/main/java/com/csr/newsfeed/ui/feed/components/list/ArticlesListComponent.kt
@@ -3,24 +3,46 @@ package com.csr.newsfeed.ui.feed.components.list
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.paging.compose.itemKey
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.csr.newsfeed.data.Article
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
-fun ArticlesListComponent(modifier: Modifier = Modifier) {
-    val article = Article(
-        title = "Title",
-        publishedAt = "December 8th, 2024"
-    )
-    LazyColumn {
-        items(10) {
-            ArticleItemComponent(article = article) {
-
+fun ArticlesListComponent(
+    modifier: Modifier = Modifier,
+    articles: LazyPagingItems<Article>,
+) {
+    when (articles.loadState.refresh) {
+        LoadState.Loading -> {
+            Text("Loading...")
+        }
+        is LoadState.Error -> {
+            Text("Error")
+        }
+        is LoadState.NotLoading -> {
+            LazyColumn {
+                items(
+                    count = articles.itemCount,
+                    key = articles.itemKey { it.uuid },
+                ) { index ->
+                    val item = articles[index]
+                    if (item != null) {
+                        ArticleItemComponent(
+                            article = item
+                        ) { }
+                        Spacer(modifier = modifier.height(16.dp))
+                    }
+                }
             }
-            Spacer(modifier = modifier.height(8.dp))
         }
     }
 }
@@ -28,5 +50,5 @@ fun ArticlesListComponent(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun ArticlesListPreview() {
-    ArticlesListComponent()
+    ArticlesListComponent(articles = flowOf(PagingData.empty<Article>()).collectAsLazyPagingItems())
 }
