d3c638eb77ca28221a0f5fff41fb9d8356a6104f
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-10 10:51:52 -0300

    INjects FeedViewModel into composable

--- app/src/main/java/com/csr/newsfeed/navigation/NavigationGraph.kt
+++ app/src/main/java/com/csr/newsfeed/navigation/NavigationGraph.kt
@@ -5,13 +5,18 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.csr.newsfeed.ui.feed.FeedScreenHoisting
+import com.csr.newsfeed.ui.feed.FeedViewModel
 import com.csr.newsfeed.ui.feed.navigation.Feed
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun NavigationGraph(navHostController: NavHostController) {
     NavHost(navController = navHostController, startDestination = Feed) {
         composable<Feed> {
-            FeedScreenHoisting()
+            val viewModel = koinViewModel<FeedViewModel>()
+            FeedScreenHoisting(
+                viewModel = viewModel
+            )
         }
     }
 }
