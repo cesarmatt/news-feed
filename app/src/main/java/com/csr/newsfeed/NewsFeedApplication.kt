425f6da72cc1ec44e1b03e00b5b97605e6c289fc
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-10 10:49:59 -0300

    Add requried code to start Koin

--- app/src/main/java/com/csr/newsfeed/NewsFeedApplication.kt
+++ app/src/main/java/com/csr/newsfeed/NewsFeedApplication.kt
@@ -1,18 +1,24 @@
 package com.csr.newsfeed
 
 import android.app.Application
+import com.csr.newsfeed.di.networkModule
+import com.csr.newsfeed.ui.feed.di.feedModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
 
 class NewsFeedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(this@NewsFeedApplication)
-            modules()
+            modules(
+                networkModule,
+                feedModule
+            )
         }
     }
 }
