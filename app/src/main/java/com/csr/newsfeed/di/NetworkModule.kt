ed323f17586b2cc7a787a933c623985dbb689590
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-10 10:51:36 -0300

    Creates network module to deal with http client and retrofit instances

--- app/src/main/java/com/csr/newsfeed/di/NetworkModule.kt
+++ app/src/main/java/com/csr/newsfeed/di/NetworkModule.kt
@@ -1,2 +1,43 @@
 package com.csr.newsfeed.di
 
+import com.csr.newsfeed.data.NewsApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+    factory {
+        val httpClient = OkHttpClient.Builder().apply {
+
+            addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+
+            connectTimeout(3, TimeUnit.MINUTES)
+            writeTimeout(3, TimeUnit.MINUTES)
+            readTimeout(3, TimeUnit.MINUTES)
+
+            addInterceptor { chain ->
+                val original = chain.request()
+                val requestBuilder = original.newBuilder().apply {
+                    addHeader("Accept", "application/json")
+                    addHeader("Content-Type", "application/json")
+                    addHeader("X-Api-Key", NewsApi.API_KEY)
+                }
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+        }.build()
+        val baseUrl = "https://newsapi.org"
+        Retrofit.Builder()
+            .client(httpClient)
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
