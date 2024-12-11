7867e4cbd0f7cd11382430d0b83a572bc91c726f
Matheus César <cesarmatt@Matheuss-MacBook-Air-2.local>
2024-12-11 09:59:48 -0300

    Adds detail to navigation graph

--- app/src/main/java/com/csr/newsfeed/navigation/NavigationGraph.kt
+++ app/src/main/java/com/csr/newsfeed/navigation/NavigationGraph.kt
@@ -4,10 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.csr.newsfeed.ui.detail.HeadlineDetailScreenHoisting
+import com.csr.newsfeed.ui.detail.HeadlineDetailViewModel
+import com.csr.newsfeed.ui.detail.data.HeadlineDetailViewObject
+import com.csr.newsfeed.ui.detail.route.Detail
 import com.csr.newsfeed.ui.feed.FeedScreenHoisting
 import com.csr.newsfeed.ui.feed.FeedViewModel
-import com.csr.newsfeed.ui.feed.navigation.Feed
+import com.csr.newsfeed.ui.feed.route.Feed
 import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
 
 @Composable
 fun NavigationGraph(navHostController: NavHostController) {
@@ -15,7 +21,25 @@ fun NavigationGraph(navHostController: NavHostController) {
         composable<Feed> {
             val viewModel = koinViewModel<FeedViewModel>()
             FeedScreenHoisting(
-                viewModel = viewModel
+                viewModel = viewModel,
+                navController = navHostController
+            )
+        }
+        composable<Detail> {
+            val args = it.toRoute<Detail>()
+            val viewModel = koinViewModel<HeadlineDetailViewModel> {
+                parametersOf(
+                    HeadlineDetailViewObject(
+                        imageUrl = args.imageUrl,
+                        title = args.title,
+                        description = args.description,
+                        content = args.content,
+                    )
+                )
+            }
+            HeadlineDetailScreenHoisting(
+                viewModel = viewModel,
+                navController = navHostController
             )
         }
     }
